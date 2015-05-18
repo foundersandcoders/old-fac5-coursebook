1609912db80ca5ebc41fed1b706259f3a3fe4bac
nikhilaravi <nikkiravi92@gmail.com>
2015-05-18 13:49:18 +0100

    Update httprequestex.js

--- week2/httprequestex.js
+++ week2/httprequestex.js
@@ -6,8 +6,8 @@ request.open("GET", 'name.txt',false); //use the open method on the HTTP object
 // setting the 3rd parameter to false means that the send method does not return until until the open request has returned. 
 // setting it to true means it runs in the background so you have to wait until you know tha status of the request is 4 (i.e. it has returned) before calling the send method
 request.send(null);
-print(request.responseText);
-print(request.statusText);
+console.log(request.responseText);
+console.log(request.statusText);
 
 
 //alternatively if the 3rd parameter in open is set to true. 
@@ -17,7 +17,7 @@ request.send(null);
 
 request.onreadystatechange = function() {
 	if(request.readyState == 4) {
-		print(request.responseText);
+		console.log(request.responseText);
 	};
 });
 
